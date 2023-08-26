038d896b6df1814aec50fbbe9fa80fbee5912746
kazim110 <kazim.mohammadi110@gmail.com>
2023-08-26 18:07:45 +0430

    Transmit the onClick function to the AddBook component

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,33 +1,18 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import { v4 as uuidv4 } from 'uuid';
-import { addBook } from '../redux/books/booksSlice';
 
-const AddBook = ({ title, author }) => {
-  const dispatch = useDispatch();
-  const addBookHandler = () => {
-    const newBook = {
-      id: uuidv4(),
-      title,
-      author,
-    };
-    dispatch(addBook(newBook));
-  };
-  return (
-    <button
-      type="button"
-      className="addBtn"
-      onClick={addBookHandler}
-    >
-      Add Book
-    </button>
-  );
-};
+const AddBook = ({ onClick }) => (
+  <button
+    type="button"
+    className="addBtn"
+    onClick={onClick}
+  >
+    Add Book
+  </button>
+);
 
 AddBook.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default AddBook;
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,13 +1,25 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import BookItem from './BookItem';
 import AddBook from './AddBook';
+import { addBook } from '../redux/books/booksSlice';
 
 const Books = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
 
   const { books } = useSelector((state) => state.books);
+
+  const dispatch = useDispatch();
+  const addBookHandler = () => {
+    const newBook = {
+      id: uuidv4(),
+      title,
+      author,
+    };
+    dispatch(addBook(newBook));
+  };
 
   return (
     <>
@@ -26,7 +38,7 @@ const Books = () => {
         <h5>Add new Book:</h5>
         <input type="text" placeholder="Book Title..." onChange={(e) => setTitle(e.target.value)} />
         <input type="text" placeholder="Author" onChange={(e) => setAuthor(e.target.value)} />
-        <AddBook title={title} author={author} />
+        <AddBook onClick={addBookHandler} />
       </form>
     </>
   );
