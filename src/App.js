aad9e277cc3b10d792303deb7aef45b387f53047
kazim110 <kazim.mohammadi110@gmail.com>
2023-09-02 23:39:50 +0430

    Delete imported App.css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import Navbar from './components/Navbar';
 import Categories from './components/Categories';
 import Books from './components/Books';
